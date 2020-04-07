6bd4fcb80a9de3a30fba7fef0bfd96d91fdb4b00
Michele Scuttari <mscuttari@users.noreply.github.com>
2020-04-07 12:44:51 +0200

    Python3 conversion

--- simulation.py
+++ simulation.py
@@ -1,32 +1,53 @@
-print "***************************************************************************************************"
-print "                                                      "
-print "				In-network data collection and processing with TinyOS.			"
-print "                                                      "
-print "***************************************************************************************************"
-
 #! /usr/bin/python
+
 from TOSSIM import *
 import sys
 
-nunNodes=14
+numNodes = 0
+topologyFile = ""
+noiseFile = ""
+
+if len(sys.argv) != 3:
+	print("Usage: python %s <nodes_amount> <topology_file> <noise_file>" % sys.argv[0])
+	sys.exit(1)
+else:
+	numNodes = sys.argv[1]
+	topologyFile = sys.argv[2]
+	noiseFile = sys.argv[3]
+
+try:
+	topology = open(topologyFile, "r")
+except IOError as e:
+    print("Can't open topology file '%s'" % topologyFile)
+	sys.exit(1)
+
+try:
+	noise = open(noiseFile, "r")
+except IOError as e:
+    print("Can't open noise file '%s'" % noiseFile)
+	sys.exit(1)
+
+print("******************************************************************************")
+print("*                                                                            *")
+print("*           In-network data collection and processing with TinyOS            *")
+print("*                                                                            *")
+print("******************************************************************************")
 
 t = Tossim([])
 r = t.radio()
 
-#Add routes between nodes from file
-f = open("topology4.txt", "r")
-for line in f:
+# Add routes between nodes from file
+for line in topology:
   s = line.split()
   if s:
-    print " src", s[0], " dest", s[1], " gain", s[2];
+    print(" src" + s[0] + " dest" + s[1] + " gain" + s[2]);
     r.add(int(s[0]), int(s[1]), float(s[2]))
 
 t.addChannel("Temp", sys.stdout)
 #t.addChannel("Boot", sys.stdout)
 t.addChannel("Failures", sys.stdout)
 
-#Add noise model from file
-noise = open("noise.txt", "r")
+# Add noise model from file
 for line in noise:
   str1 = line.strip()
   if str1:
@@ -35,19 +56,16 @@ for line in noise:
       t.getNode(i).addNoiseTraceReading(val)
 
 for i in range(1, nunNodes+1):
-  print "Creating noise model for ",i;
+  print("Creating noise model for " + i);
   t.getNode(i).createNoiseModel()
 
-#Add nodes
+# Add nodes
 for i in range(1, nunNodes+1):
   t.getNode(i).bootAtTime(100001 + i*100000);
 
 t.runNextEvent();
 time = t.time()
 
-#Show output of nodes
+# Show output of nodes
 while time + 5000000000000 > t.time():
       t.runNextEvent()
-      
-  
-print "							END OF PROGRAM					"
